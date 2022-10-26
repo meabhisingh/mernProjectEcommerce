4dc505cdf3bc6f383d206b16592e62b31b962ef7
inydv <i_nydv@hotmail.com>
2022-10-26 16:36:35 +0530

    validation error middlware

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
+  // Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message).join(", ");
+    err = new ErrorHandler(message, 400);
+  }
+
   // Wrong Mongodb Id error
   if (err.name === "CastError") {
     const message = `Resource not found. Invalid: ${err.path}`;
